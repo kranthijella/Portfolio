891c7a43d4694dcfede1ce126e498c16ea8acd0e
Kranthi Jella <kjella@csuchico.edu>
2023-12-30 01:04:56 -0800

    done with

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,168 +25,163 @@ const Contact = () => {
     const handleFocus = () => setCurrentAnimation("walk");
     const handleBlur = () => setCurrentAnimation("idle");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
         setCurrentAnimation("hit");
+        // await emailjs
+        //     .send(
+        //         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        //         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        //         {
+        //             from_name: form.name,
+        //             to_name: "JavaScript Mastery",
+        //             from_email: form.email,
+        //             to_email: "sujata@jsmastery.pro",
+        //             message: form.message,
+        //         },
+        //         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        //     )
+        //     .then(
+        //         () => {
+        //             setLoading(false);
+        //             showAlert({
+        //                 show: true,
+        //                 text: "Thank you for your message 😃",
+        //                 type: "success",
+        //             });
+        //
+        //             setTimeout(() => {
+        //                 hideAlert(false);
+        //                 setCurrentAnimation("idle");
+        //                 setForm({
+        //                     name: "",
+        //                     email: "",
+        //                     message: "",
+        //                 });
+        //             }, 3000);
+        //         },
+        //
+        //         (error) => {
+        //             setLoading(false);
+        //             console.error(error);
+        //             setCurrentAnimation("idle");
+        //
+        //             showAlert({
+        //                 show: true,
+        //                 text: "I didn't receive your message 😢",
+        //                 type: "danger",
+        //             });
+        //         }
+        //     );
+    }
 
-        emailjs
-            .send(
-                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-                {
-                    from_name: form.name,
-                    to_name: "JavaScript Mastery",
-                    from_email: form.email,
-                    to_email: "sujata@jsmastery.pro",
-                    message: form.message,
-                },
-                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-            )
-            .then(
-                () => {
-                    setLoading(false);
-                    showAlert({
-                        show: true,
-                        text: "Thank you for your message 😃",
-                        type: "success",
-                    });
+        return (
+            <div id='kittu' className='m-0'>
+                <section className='relative flex lg:flex-row  z-10 flex-col max-container '>
+                    {alert.show && <Alert {...alert} />}
 
-                    setTimeout(() => {
-                        hideAlert(false);
-                        setCurrentAnimation("idle");
-                        setForm({
-                            name: "",
-                            email: "",
-                            message: "",
-                        });
-                    }, 3000);
-                },
-                (error) => {
-                    setLoading(false);
-                    console.error(error);
-                    setCurrentAnimation("idle");
+                    <div className='flex-1 min-w-[50%] flex flex-col'>
+                        <h1 className='head-text text-blue-800 '>Get in <span className='text-[#915EFF]'>Touch</span>
+                        </h1>
 
-                    showAlert({
-                        show: true,
-                        text: "I didn't receive your message 😢",
-                        type: "danger",
-                    });
-                }
-            );
+                        <form
+                            ref={formRef}
+                            onSubmit={handleSubmit}
+                            className='w-full flex flex-col gap-7 mt-14'
+                        >
+                            <label className='text-black-500 font-semibold'>
+                                Name
+                                <input
+                                    type='text'
+                                    name='name'
+                                    className='input'
+                                    placeholder='John'
+                                    required
+                                    value={form.name}
+                                    onChange={handleChange}
+                                    onFocus={handleFocus}
+                                    onBlur={handleBlur}
+                                />
+                            </label>
+                            <label className='text-black-500 font-semibold'>
+                                Email
+                                <input
+                                    type='email'
+                                    name='email'
+                                    className='input'
+                                    placeholder='John@gmail.com'
+                                    required
+                                    value={form.email}
+                                    onChange={handleChange}
+                                    onFocus={handleFocus}
+                                    onBlur={handleBlur}
+                                />
+                            </label>
+                            <label className='text-black-500 font-semibold'>
+                                Your Message
+                                <textarea
+                                    name='message'
+                                    rows='4'
+                                    className='textarea'
+                                    placeholder='Write your thoughts here...'
+                                    value={form.message}
+                                    onChange={handleChange}
+                                    onFocus={handleFocus}
+                                    onBlur={handleBlur}
+                                />
+                            </label>
+
+                            <button
+                                type='submit'
+                                disabled={loading}
+                                className='btn'
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                            >
+                                {loading ? "Sending..." : "Submit"}
+                            </button>
+                        </form>
+                    </div>
+
+                    <div className='lg:w-[55%] w-full lg:ml-25 z-0 lg:h-auto md:h-[550px] h-[350px]'>
+                        <Canvas className="lg:ml-28"
+                                shadows
+                                frameloop='demand'
+                                dpr={[1, 2]}
+                                gl={{preserveDrawingBuffer: true}}
+                                camera={{
+                                    fov: 45,
+                                    near: 0.1,
+                                    far: 200,
+                                    position: [-4, 3, 6],
+                                }}
+                        >
+                            <directionalLight position={[0, 0, 1]} intensity={2.5}/>
+                            <ambientLight intensity={1}/>
+                            <pointLight position={[5, 10, 0]} intensity={2}/>
+                            <spotLight
+                                position={[10, 10, 10]}
+                                angle={0.15}
+                                penumbra={1}
+                                intensity={2}
+                            />
+
+                        <Suspense fallback={<Loader/>}>
+                                <OrbitControls
+                                    autoRotate
+                                    enableZoom={false}
+                                    maxPolarAngle={Math.PI / 2}
+                                    minPolarAngle={Math.PI / 2}
+                                />
+                                <primitive object={earth.scene} scale={3} position-y={0} rotation-y={0}/>
+                                <Preload all/>
+                            </Suspense>
+                        </Canvas>
+                        </div>
+                </section>
+            </div>
+        );
     };
-
-    return (
-        <div id='kittu' className='m-0'>
-            <div className={'z-30'}>
-                <NavBar color='black'/>
-            </div>
-            <StarsCanvas/>
-        <section className='relative flex lg:flex-row  z-10 flex-col max-container '>
-            {alert.show && <Alert {...alert} />}
-
-            <div className='flex-1 min-w-[50%] flex flex-col'>
-                <h1 className='head-text text-blue-800 '>Get in <span className='text-[#915EFF]'>Touch</span></h1>
-
-                <form
-                    ref={formRef}
-                    onSubmit={handleSubmit}
-                    className='w-full flex flex-col gap-7 mt-14'
-                >
-                    <label className='text-black-500 font-semibold'>
-                        Name
-                        <input
-                            type='text'
-                            name='name'
-                            className='input'
-                            placeholder='John'
-                            required
-                            value={form.name}
-                            onChange={handleChange}
-                            onFocus={handleFocus}
-                            onBlur={handleBlur}
-                        />
-                    </label>
-                    <label className='text-black-500 font-semibold'>
-                        Email
-                        <input
-                            type='email'
-                            name='email'
-                            className='input'
-                            placeholder='John@gmail.com'
-                            required
-                            value={form.email}
-                            onChange={handleChange}
-                            onFocus={handleFocus}
-                            onBlur={handleBlur}
-                        />
-                    </label>
-                    <label className='text-black-500 font-semibold'>
-                        Your Message
-                        <textarea
-                            name='message'
-                            rows='4'
-                            className='textarea'
-                            placeholder='Write your thoughts here...'
-                            value={form.message}
-                            onChange={handleChange}
-                            onFocus={handleFocus}
-                            onBlur={handleBlur}
-                        />
-                    </label>
-
-                    <button
-                        type='submit'
-                        disabled={loading}
-                        className='btn'
-                        onFocus={handleFocus}
-                        onBlur={handleBlur}
-                    >
-                        {loading ? "Sending..." : "Submit"}
-                    </button>
-                </form>
-            </div>
-
-            <div className='lg:w-[55%] w-full lg:ml-25 z-0 lg:h-auto md:h-[550px] h-[350px]'>
-                <Canvas className="lg:ml-28"
-                        shadows
-                        frameloop='demand'
-                        dpr={[1, 2]}
-                        gl={{ preserveDrawingBuffer: true }}
-                        camera={{
-                            fov: 45,
-                            near: 0.1,
-                            far: 200,
-                            position: [-4, 3, 6],
-                        }}
-                >
-                    <directionalLight position={[0, 0, 1]} intensity={2.5} />
-                    <ambientLight intensity={1} />
-                    <pointLight position={[5, 10, 0]} intensity={2} />
-                    <spotLight
-                        position={[10, 10, 10]}
-                        angle={0.15}
-                        penumbra={1}
-                        intensity={2}
-                    />
-
-                    <Suspense fallback={<Loader/>}>
-                    <Suspense fallback={<Loader />}>
-                        <OrbitControls
-                            autoRotate
-                            enableZoom={false}
-                            maxPolarAngle={Math.PI / 2}
-                            minPolarAngle={Math.PI / 2}
-                        />
-                        <primitive object={earth.scene} scale={3} position-y={0} rotation-y={0} />
-                        <Preload all />
-                    </Suspense>
-                    </Suspense>
-                </Canvas>
-            </div>
-        </section>
-        </div>
-    );
-};
 
 export default Contact;
